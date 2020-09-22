f79d058a8c8419c4cdf800e9d1423bd2597b9523
Alexis Gerardo Castellanos Mora <za20011800@zapopan.tecmm.edu.mx>
2020-09-22 16:53:11 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def resta():
+    num1= int(input("Ingresa un número: "))
+    num2= int(input("Ingresa un número: "))
+    print("La suma es: ", num1-num2)
+
+def mult():
+    num1= int(input("Ingresa un número: "))
+    num2= int(input("Ingresa un número: "))
+    print("La suma es: ", num1*num2)
+
+def div():
+    num1= int(input("Ingresa un número: "))
+    num2= int(input("Ingresa un número: "))
+    print("La suma es: ", num1/num2)
+
+def suma():
+    num1= int(input("Ingresa un número: "))
+    num2= int(input("Ingresa un número: "))
+    print("La suma es: ", num1+num2)
+
+
+if __name__ == '__main__':
+    suma()
